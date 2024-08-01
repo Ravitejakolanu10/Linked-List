6f0d6503f744854e717c2ee2e55233c1f746ddad
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-02 01:39:54 +0530

    update code create helper function to create node

--- data-structures/linear/linked-list/single-linked-list/single-lnklist.c
+++ data-structures/linear/linked-list/single-linked-list/single-lnklist.c
@@ -7,3 +7,21 @@ typedef struct Block{
     int data;
     struct Block* next;
 }node;
+
+
+node* createNode(int data); //function to create list node
+
+
+int main(){
+
+    
+    return 0;
+}
+
+node* createNode(int data){
+    node* new_node = (node*)malloc(sizeof(node));
+    new_node->data = data;
+    new_node->next = NULL;
+
+    return new_node;
+}
